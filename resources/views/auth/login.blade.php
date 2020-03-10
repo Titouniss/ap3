77c8756f2cf9896263bbdb70b7bd92534bcf40d6
Sebastien SALIDO <sebastiensalido@numidev.fr>
2020-03-10 16:57:49 +0100

    Update translation to fr

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,17 +39,17 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        {{-- <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Se souvenir de moi') }}
                                     </label>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
@@ -58,8 +58,8 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                    <a class="btn btn-link text-primary" href="{{ route('password.request') }}">
+                                        {{ __('Mot de passe oublié?') }}
                                     </a>
                                 @endif
                             </div>
