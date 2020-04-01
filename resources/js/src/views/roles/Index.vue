c723b0cac5a203c4b635e098f7fe9e5ee6cfb2da
root <root@vps804792>
2020-04-01 23:44:45 +0200

    server update

---
+++ resources/js/src/views/roles/Index.vue
@@ -0,0 +1,240 @@
+<template>
+
+  <div id="page-role-list">
+
+    <div class="vx-card p-6">
+      <div class="flex flex-wrap items-center">
+
+        <!-- ITEMS PER PAGE -->
+        <div class="flex-grow">
+          <vs-dropdown vs-trigger-click class="cursor-pointer">
+            <div class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium">
+              <span class="mr-2">{{ currentPage * paginationPageSize - (paginationPageSize - 1) }} - {{ rolesData.length - currentPage * paginationPageSize > 0 ? currentPage * paginationPageSize : rolesData.length }} of {{ rolesData.length }}</span>
+              <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
+            </div>
+            <!-- <vs-button class="btn-drop" type="line" color="primary" icon-pack="feather" icon="icon-chevron-down"></vs-button> -->
+            <vs-dropdown-menu>
+
+              <vs-dropdown-item @click="gridApi.paginationSetPageSize(10)">
+                <span>10</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="gridApi.paginationSetPageSize(20)">
+                <span>20</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="gridApi.paginationSetPageSize(25)">
+                <span>25</span>
+              </vs-dropdown-item>
+              <vs-dropdown-item @click="gridApi.paginationSetPageSize(30)">
+                <span>30</span>
+              </vs-dropdown-item>
+            </vs-dropdown-menu>
+          </vs-dropdown>
+        </div>
+
+        <!-- TABLE ACTION COL-2: SEARCH & EXPORT AS CSV -->
+          <vs-input class="sm:mr-4 mr-0 sm:w-auto w-full sm:order-normal order-3 sm:mt-0 mt-4" v-model="searchQuery" @input="updateSearchQuery" placeholder="Search..." />
+          <!-- <vs-button class="mb-4 md:mb-0" @click="gridApi.exportDataAsCsv()">Export as CSV</vs-button> -->
+
+          <!-- ACTION - DROPDOWN -->
+          <vs-dropdown vs-trigger-click class="cursor-pointer">
+
+            <div class="p-3 shadow-drop rounded-lg d-theme-dark-light-bg cursor-pointer flex items-end justify-center text-lg font-medium w-32">
+              <span class="mr-2 leading-none">Actions</span>
+              <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
+            </div>
+
+            <vs-dropdown-menu>
+
+              <vs-dropdown-item>
+                <span class="flex items-center">
+                  <feather-icon icon="TrashIcon" svgClasses="h-4 w-4" class="mr-2" />
+                  <span>Supprimer</span>
+                </span>
+              </vs-dropdown-item>
+
+              <vs-dropdown-item>
+                <span class="flex items-center">
+                  <feather-icon icon="ArchiveIcon" svgClasses="h-4 w-4" class="mr-2" />
+                  <span>Archiver</span>
+                </span>
+              </vs-dropdown-item>
+
+            </vs-dropdown-menu>
+          </vs-dropdown>
+      </div>
+
+      <div class="px-6 pb-2 pt-6">
+        <vs-button @click="addRecord" class="w-full">Ajouter un rôle</vs-button>
+      </div>
+      <!-- AgGrid Table -->
+      <ag-grid-vue
+        ref="agGridTable"
+        :components="components"
+        :gridOptions="gridOptions"
+        class="ag-theme-material w-100 my-4 ag-grid-table"
+        :columnDefs="columnDefs"
+        :defaultColDef="defaultColDef"
+        :rowData="rolesData"
+        rowSelection="multiple"
+        colResizeDefault="shift"
+        :animateRows="true"
+        :floatingFilter="false"
+        :pagination="true"
+        :paginationPageSize="paginationPageSize"
+        :suppressPaginationPanel="true"
+        :enableRtl="$vs.rtl">
+      </ag-grid-vue>
+
+      <vs-pagination
+        :total="totalPages"
+        :max="7"
+        v-model="currentPage" />
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+var model = 'role'
+var modelPlurial = 'roles'
+var modelTitle = 'Rôle'
+
+import { AgGridVue } from 'ag-grid-vue'
+import '@sass/vuexy/extraComponents/agGridStyleOverride.scss'
+import vSelect from 'vue-select'
+
+// Store Module
+import moduleManagement from '@/store/role-management/moduleRoleManagement.js'
+
+// Cell Renderer
+import CellRendererActions from './cell-renderer/CellRendererActions.vue'
+
+export default {
+  components: {
+    AgGridVue,
+    vSelect,
+    // Cell Renderer
+    CellRendererActions
+  },
+  data () {
+    return {
+      searchQuery: '',
+      // AgGrid
+      gridApi: null,
+      gridOptions: {},
+      defaultColDef: {
+        sortable: true,
+        resizable: true,
+        suppressMenu: true
+      },
+      columnDefs: [
+        {
+          filter: false,
+          checkboxSelection: true,
+          headerCheckboxSelectionFilteredOnly: false,
+          headerCheckboxSelection: true,
+          resizable: true
+        },
+        {
+          headerName: 'Titre',
+          field: 'name'
+        },
+        {
+          headerName: 'Description',
+          field: 'description'
+        },
+        {
+          headerName: 'Actions',
+          field: 'transactions',
+          cellRendererFramework: 'CellRendererActions'
+        }
+      ],
+
+      // Cell Renderer Components
+      components: {
+        CellRendererActions,
+      }
+    }
+  },
+  computed: {
+    itemIdToEdit () {
+        return this.$store.state.roleManagement.role.id || 0
+    },
+    rolesData () {
+      return this.$store.state.roleManagement.roles
+    },
+    paginationPageSize () {
+      if (this.gridApi) return this.gridApi.paginationGetPageSize()
+      else return 10
+    },
+    totalPages () {
+      if (this.gridApi) return this.gridApi.paginationGetTotalPages()
+      else return 0
+    },
+    currentPage: {
+      get () {
+        if (this.gridApi) return this.gridApi.paginationGetCurrentPage() + 1
+        else return 1
+      },
+      set (val) {
+        this.gridApi.paginationGoToPage(val - 1)
+      }
+    }
+  },
+  methods: {
+    setColumnFilter (column, val) {
+      const filter = this.gridApi.getFilterInstance(column)
+      let modelObj = null
+
+      if (val !== 'all') {
+        modelObj = { type: 'equals', filter: val }
+      }
+
+      filter.setModel(modelObj)
+      this.gridApi.onFilterChanged()
+    },
+    resetColFilters () {
+      // Reset Grid Filter
+      this.gridApi.setFilterModel(null)
+      this.gridApi.onFilterChanged()
+
+      // Reset Filter Options
+      this.roleFilter = this.statusFilter = this.isVerifiedFilter = this.departmentFilter = { label: 'All', value: 'all' }
+
+      this.$refs.filterCard.removeRefreshAnimation()
+    },
+    updateSearchQuery (val) {
+      this.gridApi.setQuickFilter(val)
+    },
+    addRecord () {
+      this.$router.push(`/${modelPlurial}/${model}-add/`).catch(() => {})
+    },
+  },
+  mounted () {
+    this.gridApi = this.gridOptions.api
+
+    /* =================================================================
+      NOTE:
+      Header is not aligned properly in RTL version of agGrid table.
+      However, we given fix to this issue. If you want more robust solution please contact them at gitHub
+    ================================================================= */
+    if (this.$vs.rtl) {
+      const header = this.$refs.agGridTable.$el.querySelector('.ag-header-container')
+      header.style.left = `-${  String(Number(header.style.transform.slice(11, -3)) + 9)  }px`
+    }
+  },
+  created () {
+    if (!moduleManagement.isRegistered) {
+      this.$store.registerModule('roleManagement', moduleManagement)
+      moduleManagement.isRegistered = true
+    }
+    this.$store.dispatch('roleManagement/fetchItems').catch(err => { console.error(err) })
+  },
+  beforeDestroy () {
+    moduleManagement.isRegistered = false
+    this.$store.unregisterModule('roleManagement')
+  }
+}
+
+</script>
