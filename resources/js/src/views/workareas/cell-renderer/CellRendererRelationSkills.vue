645c337f64a2b5f9a1488fa3f9b7c46aecea9089
unknown <maxenceferreyra@numidev.fr>
2020-05-26 15:12:47 +0200

    Fix skills display in workareas grid

--- resources/js/src/views/workareas/cell-renderer/CellRendererRelationSkills.vue
+++ resources/js/src/views/workareas/cell-renderer/CellRendererRelationSkills.vue
@@ -1,25 +1,34 @@
 <template>
-    <div class="flex items-center">
-      <span>{{ RelationToText }}</span>
-   </div>
+  <div class="flex items-center">
+    <span>{{ RelationToText }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'CellRendererVerifiedSkill',
+  name: "CellRendererVerifiedSkill",
   computed: {
-    RelationToText () {
-      let text = ''
-      if(this.params.value.length > 0){
+    RelationToText() {
+      let text = "";
+      let counter = 0;
+      if (this.params.value.length > 0) {
         this.params.value.forEach(element => {
-          if (element.name) {        
-            text = text.concat(' | ', element.name)
+          if (element.name && this.params.value.length === 1) {
+            text = element.name;
+            counter++;
+          } else if (element.name && counter === 0) {
+            text = text.concat(element.name);
+            counter++;
+          } else if (element.name) {
+            text = text.concat(" | ", element.name);
+            counter++;
           }
         });
+      } else {
+        text = "aucune";
       }
-      else { text = 'aucune' }
-      return text
+      return text;
     }
   }
-}
+};
 </script>
