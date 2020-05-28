34142f879d378cb1cca6485685b3c55ab5f07078
unknown <maxenceferreyra@numidev.fr>
2020-05-28 08:00:49 +0200

    Add range errors messages

---
+++ resources/js/src/views/ranges/errorValidForm.js
@@ -0,0 +1,14 @@
+const errorMessage = {
+    custom: {
+        name: {
+            required: "Le titre d'une gamme est requis",
+            max: "Le titre ne doit pas dépasser 255 caractères"
+        },
+        description: {
+            required: "La description d'une gamme est requis",
+            max: "La description ne doit pas dépasser 1500 caractères"
+        },
+    }
+}
+
+export default errorMessage
