440d4d6231f0448d6f83c1aaa8004c9126925919
maxence_ferreyra <maxence_ferreyra@bitbucket.org>
2020-07-21 13:59:06 +0200

    Add companyColumn to user index grid for superAdmin

--- resources/js/src/views/users/Index.vue
+++ resources/js/src/views/users/Index.vue
@@ -109,6 +109,61 @@ var model = "user";
 var modelPlurial = "users";
 var modelTitle = "Utilisateurs";
 
+var columnDef = [
+  {
+    width: 60,
+    filter: false,
+    checkboxSelection: true,
+    headerCheckboxSelectionFilteredOnly: true,
+    headerCheckboxSelection: true,
+    resizable: true
+  },
+  {
+    headerName: "Nom",
+    field: "lastname",
+    filter: true,
+    width: 200,
+    resizable: true
+  },
+  {
+    headerName: "Prénom",
+    field: "firstname",
+    filter: true,
+    width: 150,
+    resizable: true
+  },
+  {
+    headerName: "Email",
+    field: "email",
+    filter: true,
+    width: 300,
+    resizable: true
+  },
+  {
+    headerName: "Rôle",
+    field: "roles",
+    filter: true,
+    width: 150,
+    cellRendererFramework: "CellRendererRelations",
+    resizable: true
+  },
+  {
+    headerName: "Société",
+    field: "company",
+    filter: true,
+    width: 150,
+    cellRendererFramework: "CellRendererRelations",
+    resizable: true
+  },
+  {
+    headerName: "Actions",
+    field: "transactions",
+    width: 150,
+    cellRendererFramework: "CellRendererActions",
+    resizable: true
+  }
+];
+
 export default {
   components: {
     AgGridVue,
@@ -132,51 +187,7 @@ export default {
         resizable: true,
         suppressMenu: true
       },
-      columnDefs: [
-        {
-          filter: false,
-          checkboxSelection: true,
-          headerCheckboxSelectionFilteredOnly: true,
-          headerCheckboxSelection: true,
-          resizable: true
-        },
-        {
-          headerName: "Nom",
-          field: "lastname",
-          filter: true,
-          width: 200,
-          resizable: true
-        },
-        {
-          headerName: "Prénom",
-          field: "firstname",
-          filter: true,
-          width: 150,
-          resizable: true
-        },
-        {
-          headerName: "Email",
-          field: "email",
-          filter: true,
-          width: 300,
-          resizable: true
-        },
-        {
-          headerName: "Rôle",
-          field: "roles",
-          filter: true,
-          width: 150,
-          cellRendererFramework: "CellRendererRelations",
-          resizable: true
-        },
-        {
-          headerName: "Actions",
-          field: "transactions",
-          width: 150,
-          cellRendererFramework: "CellRendererActions",
-          resizable: true
-        }
-      ],
+      columnDefs: this.getColumnDef(),
 
       // Cell Renderer Components
       components: {
@@ -239,6 +250,18 @@ export default {
   methods: {
     authorizedTo(action, model = "users") {
       return this.$store.getters.userHasPermissionTo(`${action} ${model}`);
+    },
+    getColumnDef() {
+      if (
+        this.$store.state.AppActiveUser.roles.findIndex(
+          r => r.name === "superAdmin"
+        ) > -1
+      ) {
+        return columnDef;
+      } else {
+        let withoutCompany = columnDef.slice(0, 5).concat(columnDef.slice(-1));
+        return withoutCompany;
+      }
     },
     setColumnFilter(column, val) {
       const filter = this.gridApi.getFilterInstance(column);
@@ -314,6 +337,7 @@ export default {
     }
   },
   mounted() {
+    const user = this.$store.state.AppActiveUser;
     this.gridApi = this.gridOptions.api;
 
     window.addEventListener("resize", this.onResize);
--- resources/js/src/views/users/cell-renderer/CellRendererRelations.vue
+++ resources/js/src/views/users/cell-renderer/CellRendererRelations.vue
@@ -1,25 +1,34 @@
 <template>
-    <div class="flex items-center">
-      <span>{{ RelationToText }}</span>
-   </div>
+  <div class="flex items-center">
+    <span>{{ RelationToText }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'CellRendererVerified',
+  name: "CellRendererVerified",
   computed: {
-    RelationToText () {
-      let text = ''      
-      if (this.params.value && this.params.value.length) {        
-        this.params.value.forEach(element => {
-          let role = ''
-          if (text) {
-              text = ' |'+element.name
-          }else  text += element.name
-        });
+    RelationToText() {
+      let text = "";
+      if (this.params.colDef.field === "company") {
+        if (this.params.value !== null) {
+          text = this.params.value.name;
+        } else {
+          text = "Pas de société";
+        }
+      } else if (this.params.colDef.field === "roles") {
+        if (this.params.value) {
+          this.params.value.forEach(element => {
+            if (text !== "") {
+              text = " |" + element.name;
+            } else text = element.name;
+          });
+        } else {
+          text = "Pas de rôle";
+        }
       }
-      return text
+      return text;
     }
   }
-}
+};
 </script>
