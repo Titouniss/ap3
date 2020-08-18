609acb5aabbf20d7d52a7d01c737d4ed553df02b
Peter Baudry <peterbaudry@numidev.fr>
2020-08-18 13:40:02 +0200

    Customers cascade delete, archiving & restore

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -8,11 +8,38 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Customers extends Model
 {
     use SoftDeletes;
-    
-    protected $fillable = [ 'name', 'lastname', 'siret', 'professional', 'company_id'];
+
+    protected $fillable = ['name', 'lastname', 'siret', 'professional', 'company_id'];
 
     public function company()
     {
         return $this->belongsTo('App\Models\Company', 'company_id', 'id');
+    }
+
+    public function restoreCascade()
+    {
+        $projects = Project::withTrashed()->where('customer_id', $this->id)->get();
+        foreach ($projects as $project) {
+            $project->restoreCascade();
+        }
+        return $this->restore();
+    }
+
+    public function deleteCascade()
+    {
+        $projects = Project::where('customer_id', $this->id)->get();
+        foreach ($projects as $project) {
+            $project->deleteCascade();
+        }
+        return $this->delete();
+    }
+
+    public function forceDeleteCascade()
+    {
+        $projects = Project::withTrashed()->where('customer_id', $this->id)->get();
+        foreach ($projects as $project) {
+            $project->forceDeleteCascade();
+        }
+        return $this->forceDelete();
     }
 }
