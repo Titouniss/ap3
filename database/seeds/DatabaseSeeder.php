1dd216562edfb467222a149c8b0889b887563465
Peter Baudry <peterbaudry@numidev.fr>
2020-09-08 14:47:09 +0200

    Fix missing seeder error

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(CompanySeeder::class);
         $this->call(PermissionsRoleTableSeeder::class);
         $this->call(DataTypesSeeder::class);
         $this->call(DataRowsSeeder::class);
