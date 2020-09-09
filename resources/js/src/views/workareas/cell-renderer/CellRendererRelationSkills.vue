62fb3e21298a08da9020dc223ae8e150473780d9
Peter Baudry <peterbaudry@numidev.fr>
2020-09-09 11:20:35 +0200

    Fix workareas skills display

--- resources/js/src/views/workareas/cell-renderer/CellRendererRelationSkills.vue
+++ resources/js/src/views/workareas/cell-renderer/CellRendererRelationSkills.vue
@@ -1,34 +1,22 @@
 <template>
-  <div class="flex items-center">
-    <span>{{ RelationToText }}</span>
-  </div>
+    <div class="flex items-center">
+        <span>{{ RelationToText }}</span>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "CellRendererVerifiedSkill",
-  computed: {
-    RelationToText() {
-      let text = "";
-      let counter = 0;
-      if (this.params.value.length > 0) {
-        this.params.value.forEach(element => {
-          if (element.name && this.params.value.length === 1) {
-            text = element.name;
-            counter++;
-          } else if (element.name && counter === 0) {
-            text = text.concat(element.name);
-            counter++;
-          } else if (element.name) {
-            text = text.concat(" | ", element.name);
-            counter++;
-          }
-        });
-      } else {
-        text = "aucune";
-      }
-      return text;
+    name: "CellRendererVerifiedSkill",
+    computed: {
+        RelationToText() {
+            let text = "aucune";
+            if (this.params.value.length > 0) {
+                text = this.params.value
+                    .map(element => element.name)
+                    .join(" | ");
+            }
+            return text;
+        }
     }
-  }
 };
 </script>
