4061ee67cf62d6aa7b5bd1786df9979e2b1caa92
Peter Baudry <peterbaudry@numidev.fr>
2020-10-05 12:13:29 +0200

    Add company id to old id model

--- app/Models/ModelHasOldId.php
+++ app/Models/ModelHasOldId.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ModelHasOldId extends Model
 {
-    protected $fillable = ['old_id', 'new_id', 'model'];
+    protected $fillable = ['old_id', 'new_id', 'model', 'company_id'];
     protected $table = "model_has_old_id";
     public $timestamps = false;
 }
