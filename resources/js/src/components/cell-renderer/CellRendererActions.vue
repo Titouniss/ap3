2ff5e56115f29803e43abb65c54e1f4b46b88e2d
Florentin <florentinmerlet@numidev.fr>
2021-07-05 13:23:36 +0200

    Clean

--- resources/js/src/components/cell-renderer/CellRendererActions.vue
+++ resources/js/src/components/cell-renderer/CellRendererActions.vue
@@ -210,11 +210,6 @@ export default {
       }
     },
     confirmActionRecord (type) {
-      console.log(this.isAdmin && this.params.model === 'company')
-      console.log(this.isAdmin)
-      console.log(this.params.model)
-      console.log(this.params)
-      
       if (this.blockDelete && type === this.dialogTypes.delete) {
         this.$vs.dialog({
           color: 'danger',
