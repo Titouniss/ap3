0a24b648142a823601e213525e53a00212f08d69
Kevin <quiercelinkevin@gmail.com>
2022-02-14 12:09:15 +0100

    update add range in the project

--- resources/js/src/views/projects/AddForm.vue
+++ resources/js/src/views/projects/AddForm.vue
@@ -15,7 +15,7 @@
             :active.sync="activePrompt"
         >
             <div>
-                <form autocomplete="off" v-submit.prevent>
+                <form autocomplete="off">
                     <div class="vx-row">
                         <div class="vx-col w-full">
                             <div class="vx-row mt-4" v-if="!disabled">
