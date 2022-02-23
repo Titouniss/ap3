54bd86f65d0d6cd311f84c036bc7438c527bf509
Kevin <quiercelinkevin@gmail.com>
2022-02-23 17:20:11 +0100

    remove authorize supplies in the company_details

--- database/migrations/2022_02_23_171310_remove_company_details_table.php
+++ database/migrations/2022_02_23_171310_remove_company_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAuthorizeSupplyToCompanyDetailsTable extends Migration
+class RemoveCompanyDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddAuthorizeSupplyToCompanyDetailsTable extends Migration
      */
     public function up()
     {
-        Schema::table('company_details', function (Blueprint $table) {
-            $table->boolean('authorize_supply')->default(0)->after('country');
+        Schema::create('company_details', function (Blueprint $table) {
+            $table->dropColumn('authorize_supply');
         });
     }
 
